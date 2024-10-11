bd3efef67485a67471b02e03b5def4f111fab475
Venkat-Vardhan <saivenkat1579@gmail.com>
2024-10-11 19:42:50 +0530

    Completed Project

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { connectDB } from "./config/db.js"
 import path from "path"
+import dotenv from "dotenv"
 
 import productRouter from "./routes/product.route.js"
-
+dotenv.config()
 const app=express()
 app.use(express.json())
 const PORT=process.env.PORT||5000
@@ -11,7 +12,7 @@ const __dirname=path.resolve()
 
 
 app.use('/api/products',productRouter)
-if(process.env.NODE-ENV==="production"){
+if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"/frontend/dist")))
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
